3ecddae793c88e089db166774cfddf4251c4ef47
AJD <46167223+NKTJVKTU@users.noreply.github.com>
2023-12-26 01:25:47 +0300

    Update config.py

--- API/config.py
+++ API/config.py
@@ -1,7 +1,33 @@
 class Config:
-    FIRST_VDS_USER = ''
-    FIRST_VDS_PASS = '
-    AUTH_URL = f"https://my.firstvds.ru/billmgr?func=auth&username={FIRST_VDS_USER}&password={FIRST_VDS_PASS}&out=bjson"
-
-    cookies = {}  # Заполнить
-    headers = {}  # Заполнить
+    BILL_MGR_URL = "https://my.firstvds.ru/billmgr"
+    AUTH_URL = 'https://firstvds.ru/my/auth'
+    AUTH_DATA = {
+        'email': '',  #
+        'password': '',  #
+        'is_ul': False,
+        'ul_name_mounted': '',
+        'ul_name': '',
+        'realname': '',
+        'phone': '',
+        'phone_formated': '',
+        'phone_confirm': False,
+        'phone_confirm_code': '',
+        'g-recaptcha-response': '',
+        'error': False,
+        'error_text': '',
+        'form_type': 'auth',
+        'annotation_type': 'default',
+        'recaptcha': False,
+        'totp': False,
+        'totpCode': '',
+        'auth_key': '',
+        'site': '',
+        'pass_view': 'password',
+        '__device_id': '',  #
+        '__device_name': 'Chrome',
+        '__device_system': 'Windows',
+        'partner': '',
+        'sesid': '',  #
+    }
+    BOT_TOKEN = ''  #
+    DISCORD_CHANNEL_ID = 1  #
